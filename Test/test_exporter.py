4f966ce353178ae498a025645d478171e158fb3d
echo724 <eunchan1001@gmail.com>
2021-11-16 19:38:50 +0900

    Modify sequence of checking directory so block_exporter can check path exists

--- Test/test_exporter.py
+++ Test/test_exporter.py
@@ -2,12 +2,12 @@ import unittest
 import sys
 sys.path.append('../notion2md')
 sys.path.append('../Test')
+from notion2md.exporter import block_exporter
 from Test.config import specific_block_id
 
 class ExporterTest(unittest.TestCase):
     def test_runs(self):
-        from notion2md import exporter
-        exporter.block_exporter(specific_block_id)
+        block_exporter(specific_block_id)
 
 if __name__ == "__main__":
     unittest.main()
