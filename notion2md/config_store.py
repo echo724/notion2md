c12ec44cdbbe8a56b60ac6a9c87b8744ef8066e9
echo724 <eunchan1001@gmail.com>
2022-01-21 21:54:20 +0900

    refactor: Change methods of config_store.py

--- notion2md/config_store.py
+++ notion2md/config_store.py
@@ -1,6 +1,4 @@
-import os, sys, argparse
-import notion2md
-from notion2md.console import print_error,print_status
+import os
 from notion_client.helpers import get_id
 
 class Config(object):
@@ -15,27 +13,12 @@ class Config(object):
 
         self.exporter_type = type
 
-def parse_config() -> dict:
-    parser = argparse.ArgumentParser(description="Notion2md: Notion Markdown Exporter with Python Cli")
-    parser.add_argument('--type','-t',type=str,help="Set a type of target page: block, page, database",default="block")
-    parser.add_argument('--url','-u',type=str,help="Set an url of target page")
-    parser.add_argument('--id','-i',type=str,help="Set an id of target page")
-    parser.add_argument('--path','-p',type=str,help="Set a relative path of output file")
-    parser.add_argument('--name','-n',type=str,help="Set a custom name of output file")
-    parser.add_argument('--version','-v', action='store_true',help="Show a version of Notion2Md")
+config = None
 
-    return vars(parser.parse_args())
+def set_config(**kargs):
+    global config
+    config = Config(**kargs)
 
-args = parse_config()
-
-if args["version"]:
-    print_status("Version", notion2md.__version__)
-    sys.exit(None)
-else:
-    del args["version"]
-
-if not args["id"] and not args["name"]:
-    print_error("please enter a Notion page's id or url")
-    sys.exit(1)
-
-CONFIG = Config(**args)
+def get_config():
+    global config
+    return config
