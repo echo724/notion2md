4b169e2e4ac52c7fa8929d3dbdc81cf81db59110
echo724 <eunchan1001@gmail.com>
2022-02-14 19:17:25 +0900

    style: Change output style in file.py

--- notion2md/convertor/file.py
+++ notion2md/convertor/file.py
@@ -29,20 +29,15 @@ def downloader(url:str,io:IO=None) -> str:
             fullpath = os.path.join(cfg.tmp_path,downloaded_file_name)
             
             if io:
-                print_output(io,request.urlretrieve(url,fullpath),file_name,downloaded_file_name)
+                io.write_line(status("Downloading",f"{file_name}"))
+                request.urlretrieve(url,fullpath)
+                io.write_line(status("Downloaded",f'successfully downloaded "{file_name}" -> "{downloaded_file_name}"'))
             else:
                 request.urlretrieve(url,fullpath)
             return name,downloaded_file_name
         else:
             if io:
-                io.write_line(error(f"unvalid {url}"))
+                io.write_line(error(f"invalid {url}"))
     else:
         return file_name,url
-
-def print_output(io:IO,fn,file_name,downloaded_file_name):
-    try:
-        io.write_line(status("Downloading",f"{file_name}"))
-        fn()
-        io.write_line(status("Downloaded",f'successfully downloaded "{file_name}" -> "{downloaded_file_name}"'))
-    except Exception as e:
-        io.write_line(error(f"cannot download {file_name}"))
+        
