2035312cbecbdf3460110cc5f3477df609a54ef8
echo724 <eunchan1001@gmail.com>
2022-02-17 19:43:40 +0900

    refactor: Create util.py and Put zip_dir function in it

--- notion2md/console/commands/export_block.py
+++ notion2md/console/commands/export_block.py
@@ -1,27 +1,19 @@
 import os,sys
 import time
-from pathlib import Path
-from zipfile import ZIP_DEFLATED, ZipFile
-from os import PathLike
 import shutil
 
 from typing import Union
 
+from notion2md.console.ui.indicator import progress
 from notion2md.console.formatter import *
 from notion2md.config import Config
 from notion2md.notion_api import get_children
 from notion2md.convertor.block import BlockConvertor
 from notion2md.exceptions import UnInitializedConfigException
-from notion2md.console.ui.indicator import progress
+from notion2md.util import zip_dir
 
 from cleo.helpers import option
 from cleo.commands.command import Command
-
-def zip_dir(zip_name: str, source_dir: Union[str, PathLike]):
-    src_path = Path(source_dir).expanduser().resolve(strict=True)
-    with ZipFile(zip_name, 'w', ZIP_DEFLATED) as zf:
-        for file in src_path.rglob('*'):
-            zf.write(file, file.relative_to(src_path))
 
 class ExportBlockCommand(Command):
     name = "block"
