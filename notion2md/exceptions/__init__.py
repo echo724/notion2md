1dd45b12b25f6c232d0c1096486bd480b096be7e
echo724 <eunchan1001@gmail.com>
2022-03-09 14:36:10 +0900

    test: Create Exceptions for checking API Key

--- notion2md/exceptions/__init__.py
+++ notion2md/exceptions/__init__.py
@@ -1,3 +1,10 @@
-class UnInitializedConfigException(Exception):
-    def __str__(self):
-        return "Config is not initialized with values"
+class EnvVariableNotFound(Exception):
+    def __init__(self) -> None:
+        super().__init__(
+            "Envrionment Variable named as 'NOTION_TOKEN' is not found"
+        )
+
+
+class InvalidIntegrationKey(Exception):
+    def __init__(self) -> None:
+        super().__init__("Invalid Notion Integration key")
