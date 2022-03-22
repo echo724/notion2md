7b2d4375032e5dfe67843e9ccb12e8416ffffc1c
echo724 <eunchan1001@gmail.com>
2022-03-22 23:02:43 +0900

    refactor: Move notion2md exceptions to exceptions.py

--- notion2md/exceptions.py
+++ notion2md/exceptions.py
@@ -1,8 +1,6 @@
 class EnvVariableNotFound(Exception):
     def __init__(self) -> None:
-        super().__init__(
-            "Envrionment Variable named as 'NOTION_TOKEN' is not found"
-        )
+        super().__init__("Envrionment Variable 'NOTION_TOKEN' is not found")
 
 
 class InvalidIntegrationKey(Exception):
