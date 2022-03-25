d576fdf9a85516135bed04ec67725d4653a23582
echo724 <eunchan1001@gmail.com>
2022-03-25 20:20:47 +0900

    test: Add Exception cases for Notion API

--- notion2md/exceptions.py
+++ notion2md/exceptions.py
@@ -1,8 +1,21 @@
-class EnvVariableNotFound(Exception):
+class MissingTokenError(Exception):
+    """Exception for missing notion token error.
+
+    Notion2Md requires Notion Integration token key to export Notion page to Markdown.
+    The token should be saved as envronmental variable. If it is not, this exception
+    is raised.
+    """
+
     def __init__(self) -> None:
         super().__init__("Envrionment Variable 'NOTION_TOKEN' is not found")
 
 
-class InvalidIntegrationKey(Exception):
+class MissingTargetIDError(Exception):
+    """Exception for missing Notion page's id error.
+
+    Notion2Md requires url or id of Notion's page. This exception is raised if there
+    is no target id from user input.
+    """
+
     def __init__(self) -> None:
-        super().__init__("Invalid Notion Integration key")
+        super().__init__("Notion Page's id or url is not given as argument")
